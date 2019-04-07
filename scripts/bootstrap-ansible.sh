d1617e779346ebf1f5cbe15bf73fdcc546b1e357
Wong Hoi Sing Edison <hswong3i@gmail.com>
2019-04-07 23:37:20 +0800

    bootstrap pip manually

--- scripts/bootstrap-ansible.sh
+++ scripts/bootstrap-ansible.sh
@@ -4,5 +4,6 @@ set -o xtrace
 
 # Install Ansible from GIT.
 apt-get update
-apt-get -y install python-minimal python-pip python-dev
+apt-get -y install python-minimal python-dev
+curl -sL https://bootstrap.pypa.io/get-pip.py | python
 sudo -H pip install --upgrade ansible
